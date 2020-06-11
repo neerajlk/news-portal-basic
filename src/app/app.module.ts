dacef95af34000d3bb661e86a585c82417d3c0c2
Neeraj <neeraj.kamat@transerve.com>
2020-06-12 01:20:32 +0530

    hashbang for path matching at build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  APP_BASE_HREF,
+  Location,
+  LocationStrategy,
+  HashLocationStrategy,
+} from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +19,7 @@ import { NewsModule } from './news/news.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule, NewsModule],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL:'https://newsapi.org/v2/top-headlines',
+  token: 'Bearer 4413c348f94e4a36a42c88b23e9f4f4f'
 };
